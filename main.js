c9cfa852132a51da631c13228aade61aa4967d43
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-05-16 16:27:34 +0200

    Wetter Icons für die nächsten Stunden

--- main.js
+++ main.js
@@ -41,7 +41,8 @@ async function showForecast(url) {
         pointToLayer: function (feature, latlng) {
             let details = feature.properties.timeseries[0].data.instant.details;
             let time = new Date(feature.properties.timeseries[0].time);
-            console.log(time);
+
+
             let content = `
             <h4>Wettervorhersage für ${time.toLocaleString()}</h4>    
             <ul>
@@ -53,6 +54,17 @@ async function showForecast(url) {
                     <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed * 3.6)}</li>
                 </ul>       
             `;
+
+            for (let i = 0; i <= 24; i += 3) {
+                let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+                content += `<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px">`
+
+
+                console.log(i, symbol);
+
+            }
+
+
             L.popup(latlng, {
                 content: content
             }).openOn(themaLayer.forecast);
